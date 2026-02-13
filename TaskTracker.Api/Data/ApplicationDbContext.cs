ef4d17bfd4d4963978dd595ae4b37a97208b4be5
Oi Ting <oiting1218@gmail.com>
2026-02-13 18:33:08 +0800

    Add ApplicationDbContext

---
+++ TaskTracker.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTracker.Api.Models;
+
+namespace TaskTracker.Api.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
